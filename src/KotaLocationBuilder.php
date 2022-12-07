3a67c27719ff37125386007c37d53aa4ef84b47e
joviar27 <joviar27@gmail.com>
2022-12-07 14:23:47 +0700

    UPDATE FORM

---
+++ src/KotaLocationBuilder.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace ThiccPan\Larashipcost;
+
+use Illuminate\Support\Facades\Http;
+
+class KotaLocationBuilder implements LocationBuilder
+{
+    protected $id;
+
+    public function setId($id): self
+    {
+        $this->id = $id;
+
+        return $this;
+    }
+
+    public function getKota()
+    {
+        $response = Http::withHeaders([
+            'key' => $this->getKey(),
+        ])->get('https://api.rajaongkir.com/starter/city', [
+            'id' => $this->id,
+        ]);
+
+        return $response->body();
+    }
+
+  public function getAllKota()
+  {
+      $response = Http::withHeaders([
+          'key' => $this->getKey(),
+
+      ])->get('https://api.rajaongkir.com/starter/city');
+
+      return $response->body();
+  }
+
+  private function getKey()
+  {
+      return config('larashipcost.api_key');  
+  }
+}
